32dc5c89878dfd95807e973135c8b9aa4d028242
Bijoy-cloud <bishalbijoy98@gmail.com>
2023-03-15 21:13:17 +0530

    base url changed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL:'https://busy-jade-colt-belt.cyclic.app'
-    baseURL:"http://localhost:3002"
+    baseURL:'https://busy-jade-colt-belt.cyclic.app'
+    // baseURL:"http://localhost:3002"
 })
